43f30d1c3b4f173d56934cb38ede2715b06beb03
shlevanto <simo.levanto@helsinki.fi>
2022-02-05 22:33:52 +0200

    rearranged methods

--- src/main/java/BWT.java
+++ src/main/java/BWT.java
@@ -29,6 +29,74 @@ public class BWT {
 
     }
 
+    public String decodeLFMap(String s) {
+        char[] f = s.toCharArray();
+        Arrays.sort(f);
+        HashMap<Character, Integer> fCounts = new HashMap<>();
+        HashMap<String, Integer> fMap = new HashMap<>();
+
+        for (int i = 0; i < f.length; i++) {
+            char key = f[i];
+            int count = 1;
+            if (fCounts.containsKey(key)) {
+                count = fCounts.get(key);
+                count++;
+            } 
+            fCounts.put(key, count);
+
+            String indexed = key + String.valueOf(count);
+            fMap.put(indexed, i);
+        }
+        
+        char[] l = s.toCharArray();
+        int[] occurance = new int[l.length];
+        HashMap<Character, Integer> lCounts = new HashMap<>();
+
+        for (int i = 0; i < l.length; i++) {
+            char key = l[i];
+            int count = 1;
+            if (lCounts.containsKey(key)) {
+                count = lCounts.get(key);
+                count++;
+            } 
+            lCounts.put(key, count);
+            occurance[i] = count;
+        }
+
+        char[] decoded = new char[l.length];
+        int decoderIndex = decoded.length - 1; 
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(l[0]);
+        decoded[decoderIndex] = l[0];
+        decoderIndex--;
+        
+        int counter = 0;
+
+        while (true) {
+            String key = l[counter] + String.valueOf(occurance[counter]);
+            
+            if (key.equals("$1")) {
+                break;
+            }
+            
+            counter = fMap.get(key);
+            sb.append(l[counter]);
+            decoded[decoderIndex] = l[counter];
+            decoderIndex--;
+                        
+        }
+        
+        System.out.println(Arrays.toString(decoded));
+        System.out.println(Arrays.toString(l));
+        System.out.println(Arrays.toString(occurance));
+        System.out.println(fMap);
+        String result = String.valueOf(decoded);
+
+        return result.substring(1);
+
+    }
+
     public void slowEncode(String inputPath, String outputPath) {
         String s = "";
         
@@ -122,74 +190,6 @@ public class BWT {
         }
 
         return new String(b);
-
-    }
-
-    public String decodeLFMap(String s) {
-        char[] f = s.toCharArray();
-        Arrays.sort(f);
-        HashMap<Character, Integer> fCounts = new HashMap<>();
-        HashMap<String, Integer> fMap = new HashMap<>();
-
-        for (int i = 0; i < f.length; i++) {
-            char key = f[i];
-            int count = 1;
-            if (fCounts.containsKey(key)) {
-                count = fCounts.get(key);
-                count++;
-            } 
-            fCounts.put(key, count);
-
-            String indexed = key + String.valueOf(count);
-            fMap.put(indexed, i);
-        }
-        
-        char[] l = s.toCharArray();
-        int[] occurance = new int[l.length];
-        HashMap<Character, Integer> lCounts = new HashMap<>();
-
-        for (int i = 0; i < l.length; i++) {
-            char key = l[i];
-            int count = 1;
-            if (lCounts.containsKey(key)) {
-                count = lCounts.get(key);
-                count++;
-            } 
-            lCounts.put(key, count);
-            occurance[i] = count;
-        }
-
-        char[] decoded = new char[l.length];
-        int decoderIndex = decoded.length - 1; 
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(l[0]);
-        decoded[decoderIndex] = l[0];
-        decoderIndex--;
-        
-        int counter = 0;
-
-        while (true) {
-            String key = l[counter] + String.valueOf(occurance[counter]);
-            
-            if (key.equals("$1")) {
-                break;
-            }
-            
-            counter = fMap.get(key);
-            sb.append(l[counter]);
-            decoded[decoderIndex] = l[counter];
-            decoderIndex--;
-                        
-        }
-        
-        System.out.println(Arrays.toString(decoded));
-        System.out.println(Arrays.toString(l));
-        System.out.println(Arrays.toString(occurance));
-        System.out.println(fMap);
-        String result = String.valueOf(decoded);
-
-        return result.substring(1);
 
     }
 
