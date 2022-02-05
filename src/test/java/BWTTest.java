3c9952d7dd16da8d379820fb30692ca56e978db7
shlevanto <simo.levanto@helsinki.fi>
2022-02-05 23:22:19 +0200

    unit test for BWT

--- src/test/java/BWTTest.java
+++ src/test/java/BWTTest.java
@@ -1,44 +1,20 @@
-import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.io.FileWriter;
-
 public class BWTTest {
-    private FileIO io;
-    private RLE rle;
+    private BWT bwt;
 
     public BWTTest() {
-        this.io = new FileIO();
-        this.rle = new RLE(io);
-        try {
-            FileWriter myWriter = new FileWriter("testfile.txt");
-            myWriter.write("aababa");
-            myWriter.close();
-        } catch (Exception e) {
-        }
-    }
-    
-    @After
-    public void tearDownClass() {
-        File toDelOne = new File("testfile.txt");
-        File toDelTwo = new File("output.txt");
-        try {
-            toDelOne.delete();
-            toDelTwo.delete();
-        } catch (Exception e) {
-            System.out.println("No test files found.");
-        }
+        this.bwt = new BWT();
     }
 
     @Test
-    public void encodeDecodeTest() {
-        rle.encode("testfile.txt", "output.txt");
-        File file = new File("output.txt");
-        assertTrue(file.exists());
-        String decoded = rle.decode("output.txt");
-        assertTrue(decoded.equals("aababa"));    
+    public void simpleEncodeDecodeTest() {
+        String s = "Is this the real life?";
+        String encoded = bwt.encode(s);
+        String decoded = bwt.decode(encoded);
+        
+        assertEquals(decoded, s);
     }
 
 }
